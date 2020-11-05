5181d86ace3e23571537af2c4ffd738614dbb9d8
Bsl-java <1185923195qq@qq.com>
2020-11-05 21:12:28 +0800

    测试小demo

--- src/main/java/com/offcn/springgbootthymeleafdemo/controller/TestController3.java
+++ src/main/java/com/offcn/springgbootthymeleafdemo/controller/TestController3.java
@@ -3,6 +3,7 @@ package com.offcn.springgbootthymeleafdemo.controller;
 public class TestController3 {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello");
         System.out.println("你好");
         System.out.println("12345423435");
     }
